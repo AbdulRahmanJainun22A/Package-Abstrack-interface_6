b4719b05c6e5a8579d946dbdee89f472f527c394
AbdulRahmanJainun22A <146951521+AbdulRahmanJainun22A@users.noreply.github.com>
2024-04-12 10:04:56 +0700

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Abdul Rahman Jainun
+ */
+public interface Laptop {
+    int MAX_VOL = 100;
+    int MIN_VOL = 0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+    
+}
